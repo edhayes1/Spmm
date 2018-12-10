3b49148445e85e9de0b47cb801ffb2b5706d1e51
Ed Hayes <edhayes1000@aol.com>
2018-12-10 00:52:42 +0000

    better structure, variable names and comments

--- optimised-sparsemm.c
+++ optimised-sparsemm.c
@@ -15,37 +15,41 @@ void spgemm(const CSR A, const CSR B, CSR C){
     int n = C->n;
     int nnz_counter = 0;
 
-    // temp accumulates a column of the product
+    // temp accumulates a row of the product
     double * temp = calloc(n, sizeof(double));
 
-    // next keeps track of where we are in the column comp. - init to -1.
+    // next keeps track of which columns have entries in - init to -1
     int * next = malloc(n * sizeof(int));
     memset(next, -1, n*sizeof(int));
 
+    //first in row pointer is always 0
     C->row_start[0] = 0;
 
     for (int i = 0; i < m; i++){
-        int col_start = -2;
-        int length = 0;
+        int pos = -2;
+        int nnz_row = 0;
 
+        // loop over columns + data in row of A
         for (int n = A->row_start[i]; n < A->row_start[i+1]; n++){
             int j = A->col_indices[n];
             double x = A->data[n];
 
+            // loop over each row of B that line up with the column j
             for (int k = B->row_start[j]; k < B->row_start[j+1]; k++) {
                 int k_col = B->col_indices[k];
                 temp[k_col] += x * B->data[k];
 
+                //if product doesnt have element in column, add it
                 if (next[k_col] == -1){
-                    next[k_col] = col_start;
-                    col_start = k_col;
-                    length++;
+                    next[k_col] = pos;
+                    pos = k_col;
+                    nnz_row++;
                 }
             }
         }
 
-        // if we underestimated, allocate twice the memory
-        if (nnz_counter + length > C->NZ){
+        // if we underestimated the number of non zeros, allocate twice the memory
+        if (nnz_counter + nnz_row > C->NZ){
             printf("re estimating NZ in product\n");
             int NZ_estimate = 2 * C->NZ;
             int * realloc_col_indices = realloc(C->col_indices, NZ_estimate * sizeof(int));
@@ -63,19 +67,22 @@ void spgemm(const CSR A, const CSR B, CSR C){
             }
         }
 
-        for (int cj = 0; cj < length; cj++){
-            if(temp[col_start] != 0){
-                C->col_indices[nnz_counter] = col_start;
-                C->data[nnz_counter] = temp[col_start];
+        // accumulate - follow next through to get the right values in temp!
+        for (int cj = 0; cj < nnz_row; cj++){
+            if(temp[pos] != 0){
+                C->col_indices[nnz_counter] = pos;
+                C->data[nnz_counter] = temp[pos];
                 nnz_counter++;
             }
 
-            int t = col_start;
-            col_start = next[col_start];
+            // reset next and temp for each row
+            int t = pos;
+            pos = next[pos];
             next[t] = -1;
             temp[t] = 0;
         }
 
+        // update the row pointer
         C->row_start[i+1] = nnz_counter;
 
     }
@@ -100,6 +107,7 @@ void optimised_sparsemm_CSR(const CSR A, const CSR B, CSR *C){
  */
 void optimised_sparsemm(const COO A, const COO B, COO *C)
 {
+    // convert input to csr
     CSR A_csr, B_csr, C_csr;
     coo_to_csr(A, &A_csr);
     coo_to_csr(B, &B_csr);
@@ -107,12 +115,14 @@ void optimised_sparsemm(const COO A, const COO B, COO *C)
     // get dimensions of C
     int C_m = A->m;
     int C_n = B->n;
-    // estimate number of NZ
+
+    // estimate number of NZ (2 times the sum of nz input)
     int C_nnz = 2*(A->NZ + B->NZ);
     alloc_sparse_CSR(C_m, C_n, C_nnz, &C_csr);
 
     spgemm(A_csr, B_csr, C_csr);
 
+    //convert product back to COO, free CSRs
     csr_to_coo(C_csr, C);
     free_CSR(&A_csr);
     free_CSR(&B_csr);
@@ -121,66 +131,69 @@ void optimised_sparsemm(const COO A, const COO B, COO *C)
 
 
 void sum(const CSR mat_1, const CSR mat_2, const CSR mat_3, CSR sum){
-    int num_rows = sum->m;
-    int num_cols = sum->n;
+    int m = sum->m;
+    int n = sum->n;
     int nnz_counter = 0;
 
 
-    double * temp = calloc(num_cols, sizeof(double));
-    int * next = malloc(num_cols * sizeof(int));
-    memset(next, -1, num_cols * sizeof(int));
+    double * temp = calloc(n, sizeof(double));
+    int * next = malloc(n * sizeof(int));
+    memset(next, -1, n * sizeof(int));
     sum->row_start[0] = 0;
 
     // loop over each row
-    for (int m = 0; m < num_rows; m++){
+    for (int i = 0; i < m; i++){
 
-        int col_start = -2;
-        int length = 0;
+        int pos = -2;
+        int row_nz = 0;
 
-        for (int col_ind = mat_1->row_start[m]; col_ind < mat_1->row_start[m+1]; col_ind++){
+        // similar premise to multiplication, but instead we are adding to temp instead of +=
+        // one loop for each matrix adding to temp
+        for (int col_ind = mat_1->row_start[i]; col_ind < mat_1->row_start[i+1]; col_ind++){
             int col = mat_1->col_indices[col_ind];
             temp[col] = mat_1->data[col_ind];
             if (next[col] == -1){
-                next[col] = col_start;
-                col_start = col;
-                length++;
+                next[col] = pos;
+                pos = col;
+                row_nz++;
             }
         }
 
-        for (int col_ind = mat_2->row_start[m]; col_ind < mat_2->row_start[m+1]; col_ind++){
+        for (int col_ind = mat_2->row_start[i]; col_ind < mat_2->row_start[i+1]; col_ind++){
             int col = mat_2->col_indices[col_ind];
             temp[col] += mat_2->data[col_ind];
             if (next[col] == -1){
-                next[col] = col_start;
-                col_start = col;
-                length++;
+                next[col] = pos;
+                pos = col;
+                row_nz++;
             }
         }
 
-        for (int col_ind = mat_3->row_start[m]; col_ind < mat_3->row_start[m+1]; col_ind++){
+        for (int col_ind = mat_3->row_start[i]; col_ind < mat_3->row_start[i+1]; col_ind++){
             int col = mat_3->col_indices[col_ind];
             temp[col] += mat_3->data[col_ind];
             if (next[col] == -1){
-                next[col] = col_start;
-                col_start = col;
-                length++;
+                next[col] = pos;
+                pos = col;
+                row_nz++;
             }
         }
 
-        for (int cj = 0; cj < length; cj++){
-            if(temp[col_start] != 0){
-                sum->col_indices[nnz_counter] = col_start;
-                sum->data[nnz_counter] = temp[col_start];
+        // row is accumulated in same way as multiplication
+        for (int cj = 0; cj < row_nz; cj++){
+            if(temp[pos] != 0){
+                sum->col_indices[nnz_counter] = pos;
+                sum->data[nnz_counter] = temp[pos];
                 nnz_counter++;
             }
 
-            int t = col_start;
-            col_start = next[col_start];
+            int t = pos;
+            pos = next[pos];
             next[t] = -1;
             temp[t] = 0;
         }
 
-        sum->row_start[m+1] = nnz_counter;
+        sum->row_start[i+1] = nnz_counter;
 
     }
 
@@ -194,6 +207,7 @@ void optimised_sparsemm_sum(const COO A, const COO B, const COO C,
                             const COO D, const COO E, const COO F,
                             COO *O)
 {
+    // convert input to csr
     CSR A_csr, B_csr, C_csr, D_csr, E_csr, F_csr, ABC, DEF, ret;
     coo_to_csr(A, &A_csr);
     coo_to_csr(B, &B_csr);
@@ -202,6 +216,7 @@ void optimised_sparsemm_sum(const COO A, const COO B, const COO C,
     coo_to_csr(E, &E_csr);
     coo_to_csr(F, &F_csr);
 
+    // estimate num non zeros - maximum can be sum of input NZ
     int nnz_ABC = A_csr->NZ + B_csr->NZ + C_csr->NZ;
     int nnz_DEF = D_csr->NZ + E_csr->NZ + F_csr->NZ;
 
@@ -213,6 +228,7 @@ void optimised_sparsemm_sum(const COO A, const COO B, const COO C,
 
     optimised_sparsemm_CSR(ABC, DEF, &ret);
 
+    // convert product back to COO. Free CSRs
     csr_to_coo(ret, O);
 
     free_CSR(&A_csr);
